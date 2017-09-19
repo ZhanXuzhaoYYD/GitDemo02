f0087cc8e5f2807db7ebb7eb7a4807ac26a54ca5
zhanxuzhao <zhanxuzhao@yydrobot.com>
2017-09-19 18:20:18 +0800

    20

--- app/src/main/java/com/yyd/gitdemo02/MainActivity.java
+++ app/src/main/java/com/yyd/gitdemo02/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 1730
         // 1805
-        //06
+        // 06
+        // 20
     }
 }
